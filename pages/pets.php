a654111b8fe198c7abca02c9a976d7a1cb597805
eduardo <edunegreiross@gmail.com>
2024-09-06 22:07:58 -0300

    Inserção da tabela pets

--- pages/pets.php
+++ pages/pets.php
@@ -1,1 +1,25 @@
 <h3>Pets</h3>
+
+<table>
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Raça</th>
+            <th>Sexo</th>
+            <th>Data de Nasc.</th>
+        </tr>
+    </thead>
+
+    <tbody>
+        <tr>
+
+            <td>5858</td>
+            <td>Thor</td>
+            <td>Labrador</td>
+            <td>M</td>
+            <td>11/04/2019</td>
+
+        </tr>
+    </tbody>
+</table>
