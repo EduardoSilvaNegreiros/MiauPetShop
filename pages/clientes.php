5a4ca9f735713750fc0aa508a8bff96dab06e26c
eduardo <edunegreiross@gmail.com>
2024-09-10 10:00:11 -0300

    Atualização clientes.php

--- pages/clientes.php
+++ pages/clientes.php
@@ -12,24 +12,46 @@
             <th>Telefone</th>
             <th>Sexo</th>
             <th>Data de Nasc.</th>
+            <th>Cpf</th>
         </tr>
     </thead>
-    <?php
-    // Definindo uma variável chamada sql para poder selecionar todos os campos da tabela no banco de dados e colocar dentro da do nosso site
-    $sql = "SELECT * FROM tbclientes";
-    // Executa a consulta SQL e, em caso de erro, exibe uma mensagem e interrompe a execução. O rs é uma abreviação de "result set" (conjunto de resultados), e muitos desenvolvedores a usam para lembrar que aquela variável armazena os resultados de uma consulta SQL
-    $rs = mysqli_query($conexao, $sql) or die("Erro ao executar a consulta" . mysqli_error($conexao));
-    ?>
     <tbody>
-        <tr>
+        <?php
+        // Definindo uma variável chamada sql para poder selecionar todos os campos da tabela no banco de dados e colocar dentro da do nosso site
+        $sql = "SELECT * FROM tbclientes";
+        // Executa a consulta SQL e, em caso de erro, exibe uma mensagem e interrompe a execução. O rs é uma abreviação de "result set" (conjunto de resultados), e muitos desenvolvedores a usam para lembrar que aquela variável armazena os resultados de uma consulta SQL
+        $rs = mysqli_query($conexao, $sql) or die("Erro ao executar a consulta!" . mysqli_error($conexao));
+        // Inicia um loop para percorrer todos os resultados retornados pela consulta SQL
+        while ($dados = mysqli_fetch_assoc($rs)) {
+            // A função mysqli_fetch_assoc() pega cada linha de resultado como um array associativo,
+            // onde as chaves são os nomes das colunas da tabela e os valores são os dados.
+        ?>
+            <tr>
 
-            <td>5858</td>
-            <td>Eduardo</td>
-            <td>edunegreiross@gmail.com</td>
-            <td>(11) 99225-2201</td>
-            <td>Masculino</td>
-            <td>27/03/2006</td>
+                <!-- Exibe o valor do campo "idCliente" da linha atual dentro de uma célula da tabela -->
+                <td><?php echo $dados["idCliente"] ?></td>
 
-        </tr>
+                <!-- Exibe o valor do campo "nomeCliente" -->
+                <td><?php echo $dados["nomeCliente"] ?></td>
+
+                <!-- Exibe o valor do campo "emailCliente" -->
+                <td><?php echo $dados["emailCliente"] ?></td>
+
+                <!-- Exibe o valor do campo "telefoneCliente" -->
+                <td><?php echo $dados["telefoneCliente"] ?></td>
+
+                <!-- Exibe o valor do campo "sexoCliente" -->
+                <td><?php echo $dados["sexoCliente"] ?></td>
+
+                <!-- Exibe o valor do campo "dataNascCliente" -->
+                <td><?php echo $dados["dataNascCliente"] ?></td>
+
+                <!-- Exibe o valor do campo "cpfCliente" -->
+                <td><?php echo $dados["cpfCliente"] ?></td>
+            </tr>
+        <?php
+            // Fecha o loop, voltando ao início para pegar a próxima linha de resultado, até que não haja mais dados.
+        }
+        ?>
     </tbody>
 </table>
