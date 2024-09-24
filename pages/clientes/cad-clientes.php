d2690e549ecad3b01c51d376382ca4e54cf4bc9a
eduardo <edunegreiross@gmail.com>
2024-09-24 11:48:13 -0300

    Atualização

--- pages/clientes/cad-clientes.php
+++ pages/clientes/cad-clientes.php
@@ -1,41 +1,33 @@
-<!-- Cabeçalho do formulário, onde é exibido o título "Cadastro de Contato" -->
-<header class="form-header">
-    <h3>Cadastro de Contato</h3>
-</header>
+<div class="container">
+    <div class="form">
+        <header class="form-header">
+            <h3>Cadastro de Contato</h3>
+        </header>
+        <form action="" method="post">
+            <div class="input-group">
+                <div class="input-box">
+                    <label for="cpfCliente">Cpf</label>
+                    <input type="text" name="cpfCliente">
 
-<!-- Início da div que envolve o formulário -->
-<div class="form">
-    <!-- Início do formulário, com o método POST (para envio de dados) e sem ação definida -->
-    <form action="" method="post">
-        <!-- Grupo de inputs (campos do formulário) -->
-        <div class="input-group">
-            <!-- Caixa de entrada de dados dentro do grupo -->
-            <div class="input-box">
-                <!-- Rótulo para o campo de cpf do cliente -->
-                <label for="cpfCliente">Cpf</label>
-                <!-- Campo de entrada de texto para o cpf do cliente -->
-                <input type="text" name="cpfCliente">
+                    <label for="dataNascCliente">Data de Nascimento</label>
+                    <input type="text" name="dataNascCliente">
 
-                <!-- Rótulo para o campo de e-mail do cliente -->
-                <label for="dataNascCliente">Data de Nascimento</label>
-                <!-- Campo de entrada de texto para o e-mail do cliente -->
-                <input type="text" name="dataNascCliente">
+                    <label for="emailCliente">E-mail</label>
+                    <input type="text" name="emailCliente">
 
-                <label for="emailCliente">E-mail</label>
-                <input type="text" name="emailCliente">
+                    <label for="nomeCliente">Nome</label>
+                    <input type="text" name="nomeCliente">
 
-                <label for="nomeCliente">Nome</label>
-                <input type="text" name="nomeCliente">
+                    <label for="sexoCliente">Sexo</label>
+                    <input type="text" name="sexoCliente">
 
-                <label for="sexoCliente">Sexo</label>
-                <input type="text" name="sexoCliente">
-
-                <label for="dataNascCliente">Telefone</label>
-                <input type="text" name="telefoneCliente">
+                    <label for="telefoneCliente">Telefone</label>
+                    <input type="text" name="telefoneCliente">
+                </div>
             </div>
-        </div>
-        <div>
-            <input type="button" value="Adicionar" name="btnAdicionar">
-        </div>
-    </form>
+            <div>
+                <input type="button" value="Adicionar" name="btnAdicionar">
+            </div>
+        </form>
+    </div>
 </div>
