692d2a3996257a8bfe5ec4b7d29efe9153b37c39
eduardo <edunegreiross@gmail.com>
2024-09-24 12:39:36 -0300

    Atualização do código

--- pages/clientes/cad-clientes.php
+++ pages/clientes/cad-clientes.php
@@ -1,48 +1,46 @@
 <div class="container">
-    <div class="form">
-        <header class="form-header">
-            <h3>Cadastro de Contato</h3>
-        </header>
-        <form action="" method="post">
-            <div class="input-group">
-                <div class="input-box">
-                    <label for="cpfCliente">CPF</label>
-                    <input type="text" name="cpfCliente" id="cpfCliente" required>
-                </div>
-
-                <div class="input-box">
-                    <label for="dataNascCliente">Data de Nascimento</label>
-                    <input type="date" name="dataNascCliente" id="dataNascCliente" required>
-                </div>
-
-                <div class="input-box">
-                    <label for="emailCliente">E-mail</label>
-                    <input type="email" name="emailCliente" id="emailCliente" required>
-                </div>
-
-                <div class="input-box">
-                    <label for="nomeCliente">Nome</label>
-                    <input type="text" name="nomeCliente" id="nomeCliente" required>
-                </div>
-
-                <div class="input-box">
-                    <label for="sexoCliente">Sexo</label>
-                    <select name="sexoCliente" id="sexoCliente" required>
-                        <option value="" disabled selected>Selecione</option>
-                        <option value="masculino">Masculino</option>
-                        <option value="feminino">Feminino</option>
-                        <option value="outro">Outro</option>
-                    </select>
-                </div>
-
-                <div class="input-box">
-                    <label for="telefoneCliente">Telefone</label>
-                    <input type="text" name="telefoneCliente" id="telefoneCliente" required>
-                </div>
+    <header class="form-header">
+        <h3>Cadastro de Contato</h3>
+    </header>
+    <form action="" method="post">
+        <div class="input-group">
+            <div class="input-box">
+                <label for="cpfCliente">CPF</label>
+                <input type="text" name="cpfCliente" id="cpfCliente" required>
             </div>
-            <div>
-                <input type="submit" value="Adicionar" name="btnAdicionar">
+
+            <div class="input-box">
+                <label for="dataNascCliente">Data de Nascimento</label>
+                <input type="date" name="dataNascCliente" id="dataNascCliente" required>
             </div>
-        </form>
-    </div>
+
+            <div class="input-box">
+                <label for="emailCliente">E-mail</label>
+                <input type="email" name="emailCliente" id="emailCliente" required>
+            </div>
+
+            <div class="input-box">
+                <label for="nomeCliente">Nome</label>
+                <input type="text" name="nomeCliente" id="nomeCliente" required>
+            </div>
+
+            <div class="input-box">
+                <label for="sexoCliente">Sexo</label>
+                <select name="sexoCliente" id="sexoCliente" required>
+                    <option value="" disabled selected>Selecione</option>
+                    <option value="masculino">Masculino</option>
+                    <option value="feminino">Feminino</option>
+                    <option value="outro">Outro</option>
+                </select>
+            </div>
+
+            <div class="input-box">
+                <label for="telefoneCliente">Telefone</label>
+                <input type="text" name="telefoneCliente" id="telefoneCliente" required>
+            </div>
+        </div>
+        <div>
+            <input type="submit" value="Adicionar" name="btnAdicionar">
+        </div>
+    </form>
 </div>
