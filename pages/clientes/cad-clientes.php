177a41d4785525b484352d89cfc24b5d4ae71f47
eduardo <edunegreiross@gmail.com>
2024-09-24 11:31:51 -0300

    Atualização cad-clientes.php

--- pages/clientes/cad-clientes.php
+++ pages/clientes/cad-clientes.php
@@ -17,9 +17,9 @@
                 <input type="text" name="nomeCliente">
 
                 <!-- Rótulo para o campo de e-mail do cliente -->
-                <label for="emailCliente">E-mail</label>
+                <label for="dataCliente">E-mail</label>
                 <!-- Campo de entrada de texto para o e-mail do cliente -->
-                <input type="email" name="emailCliente">
+                <input type="data" name="emailCliente">
 
                 <!-- Repetição desnecessária do rótulo e do campo de e-mail -->
                 <!-- Este bloco parece ser um erro, pois o rótulo e o campo já foram declarados anteriormente -->
@@ -42,20 +42,27 @@
         <div class="input-group">
             <!-- Caixa de entrada de dados dentro do grupo -->
             <div class="input-box">
-                <!-- Rótulo para o campo de nome do cliente -->
-                <label for="nomeCliente">Nome</label>
-                <!-- Campo de entrada de texto para o nome do cliente -->
-                <input type="text" name="nomeCliente">
+                <!-- Rótulo para o campo de cpf do cliente -->
+                <label for="cpfCliente">Cpf</label>
+                <!-- Campo de entrada de texto para o cpf do cliente -->
+                <input type="text" name="cpfCliente">
 
                 <!-- Rótulo para o campo de e-mail do cliente -->
-                <label for="emailCliente">E-mail</label>
+                <label for="dataNascCliente">Data de Nascimento</label>
                 <!-- Campo de entrada de texto para o e-mail do cliente -->
-                <input type="email" name="emailCliente">
+                <input type="text" name="dataNascCliente">
 
-                <!-- Repetição desnecessária do rótulo e do campo de e-mail -->
-                <!-- Este bloco parece ser um erro, pois o rótulo e o campo já foram declarados anteriormente -->
                 <label for="emailCliente">E-mail</label>
-                <input type="email" name="emailCliente">
+                <input type="text" name="emailCliente">
+
+                <label for="nomeCliente">Nome</label>
+                <input type="text" name="nomeCliente">
+
+                <label for="sexoCliente">Sexo</label>
+                <input type="text" name="sexoCliente">
+
+                <label for="dataNascCliente">Telefone</label>
+                <input type="text" name="telefoneCliente">
             </div>
         </div>
     </form>
