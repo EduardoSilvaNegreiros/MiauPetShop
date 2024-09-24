a74c1e850720ff864de786d750466d79eb3f1752
eduardo <edunegreiross@gmail.com>
2024-09-23 22:29:46 -0300

    Atuaização do código

--- pages/pets.php
+++ pages/pets.php
@@ -1,8 +1,12 @@
+<!-- Título da seção -->
 <h3>Pets</h3>
 
+<!-- Início da tabela que contém informações dos pets -->
 <table>
+    <!-- Cabeçalho da tabela, define os títulos de cada coluna -->
     <thead>
         <tr>
+            <!-- Cada 'th' representa um título de coluna -->
             <th>ID</th>
             <th>Nome</th>
             <th>Raça</th>
@@ -11,15 +15,16 @@
         </tr>
     </thead>
 
+    <!-- Corpo da tabela, onde os dados reais dos pets serão exibidos -->
     <tbody>
+        <!-- Início de uma linha de dados -->
         <tr>
-
-            <td>5858</td>
-            <td>Thor</td>
-            <td>Labrador</td>
-            <td>M</td>
-            <td>11/04/2019</td>
-
+            <!-- Cada 'td' representa uma célula da linha com os dados dos pets -->
+            <td>5858</td> <!-- ID do pet -->
+            <td>Thor</td> <!-- Nome do pet -->
+            <td>Labrador</td> <!-- Raça do pet -->
+            <td>M</td> <!-- Sexo do pet (M para macho) -->
+            <td>11/04/2019</td> <!-- Data de nascimento do pet -->
         </tr>
     </tbody>
 </table>
