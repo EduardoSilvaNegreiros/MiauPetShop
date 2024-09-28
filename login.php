67f30597cfd12f0748e2b95991f72d5bd900cbd1
eduardo <edunegreiross@gmail.com>
2024-09-28 19:29:00 -0300

    Atualização login.php

--- login.php
+++ login.php
@@ -2,19 +2,15 @@
 <html lang="pt-BR">
 
 <head>
-    <!-- Define a codificação de caracteres para UTF-8 -->
     <meta charset="UTF-8" />
-    <!-- Viewport para dispositivos móveis, ajusta o layout -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <!-- Inclui o arquivo CSS externo para estilizar o formulário de login -->
     <link rel="stylesheet" href="assets/css/login.css" />
-    <!-- Título da aba do navegador -->
     <title>Login - Pet Shop</title>
 </head>
 
 <body>
 
-    <!-- Container principal da página -->
+    <!-- Container principal -->
     <div class="container">
 
         <!-- Seção de imagem do logo -->
@@ -22,27 +18,23 @@
             <img src="assets/img/logo-miau-pet-1.svg" alt="imagemPetShop" />
         </div>
 
-        <!-- Seção do formulário -->
+        <!-- Formulário de login -->
         <div class="form">
-            <!-- Definição do formulário com o método POST -->
             <form action="./process/process_login.php" method="POST">
 
-                <!-- Cabeçalho do formulário -->
-                <div class=" form-header">
+                <div class="form-header">
                     <div class="title">
                         <h1>Login</h1>
                     </div>
                 </div>
 
-                <!-- Grupo de inputs para e-mail e senha -->
+                <!-- Campos de e-mail e senha -->
                 <div class="input-group">
-                    <!-- Input para o e-mail -->
                     <div class="input-box">
                         <label for="email">Seu e-mail</label>
                         <input type="email" name="email" id="email" placeholder="Digite seu e-mail" required />
                     </div>
 
-                    <!-- Input para a senha -->
                     <div class="input-box">
                         <label for="password">Senha</label>
                         <input type="password" name="password" id="password" placeholder="Digite sua senha" required />
@@ -60,7 +52,7 @@
                 </div>
             </form>
 
-            <!-- Mensagem com link para página de cadastro -->
+            <!-- Link para página de cadastro -->
             <div class="register-message">
                 <p>
                     Não tem login?
