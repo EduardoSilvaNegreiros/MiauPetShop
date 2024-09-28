88c48d403c5e1158b1ecf5bdf979047e4d6b02e0
eduardo <edunegreiross@gmail.com>
2024-09-28 19:32:55 -0300

    Atualização conexao.php

--- database/conexao.php
+++ database/conexao.php
@@ -1,11 +1,5 @@
 <?php 
-// Este arquivo faz a conexão com o banco de dados para o sistema do Pet Shop.
-
-// Inclui o arquivo 'config.php', que contém as configurações de conexão (constantes para servidor, usuário, senha, etc.)
+// Conexão com o banco de dados
 include("config.php");
-
-// A variável '$conexao' estabelece a conexão com o banco de dados.
-// Ela utiliza a função 'mysqli_connect()', passando os parâmetros definidos no arquivo 'config.php' (SERVIDOR, USUARIO, SENHA, BANCO).
-// Caso ocorra um erro de conexão, a função 'die()' será executada, interrompendo o script e exibindo uma mensagem de erro com o detalhe da falha.
-$conexao = mysqli_connect(SERVIDOR, USUARIO, SENHA, BANCO) or die("Erro na conexão com o servidor: " . mysqli_connect_error());
+$conexao = mysqli_connect(SERVIDOR, USUARIO, SENHA, BANCO) or die("Erro na conexão: " . mysqli_connect_error());
 ?>
