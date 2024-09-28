3c6e1804f3c68fcca0729d4be24afac0a6584486
eduardo <edunegreiross@gmail.com>
2024-09-28 19:31:48 -0300

    Atualização config.php

--- database/config.php
+++ database/config.php
@@ -1,15 +1,7 @@
 <?php
-// Definição das constantes para configuração da conexão com o banco de dados
-
-// SERVIDOR: Define o endereço do servidor de banco de dados. No caso, "localhost" indica que o banco está hospedado localmente.
+// Configurações de conexão com o banco de dados
 define("SERVIDOR", "localhost");
-
-// USUARIO: Define o nome de usuário usado para se conectar ao banco de dados. Neste exemplo, "root" é o nome de usuário padrão para o MySQL em ambiente local.
 define("USUARIO", "root");
-
-// SENHA: Define a senha usada para autenticação do usuário no banco de dados. Aqui está vazia, pois em muitos ambientes locais o usuário "root" não tem senha.
 define("SENHA", "");
-
-// BANCO: Define o nome do banco de dados que será acessado. Neste caso, "dbpetshop" é o nome do banco de dados utilizado pelo sistema do Pet Shop.
 define("BANCO", "dbpetshop");
 ?>
