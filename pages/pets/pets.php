4da25926acc3723ddadd5c607ed617256823e7ff
eduardo <edunegreiross@gmail.com>
2024-09-28 19:37:52 -0300

    Atualização pets.php

--- pages/pets/pets.php
+++ pages/pets/pets.php
@@ -6,12 +6,10 @@
     <a href="index.php?menuop=cad-pets">Novo Pet</a>
 </div>
 
-<!-- Início da tabela que contém informações dos pets -->
+<!-- Tabela de pets -->
 <table>
-    <!-- Cabeçalho da tabela, define os títulos de cada coluna -->
     <thead>
         <tr>
-            <!-- Cada 'th' representa um título de coluna -->
             <th>ID</th>
             <th>Nome</th>
             <th>Raça</th>
@@ -19,17 +17,13 @@
             <th>Data de Nasc.</th>
         </tr>
     </thead>
-
-    <!-- Corpo da tabela, onde os dados reais dos pets serão exibidos -->
     <tbody>
-        <!-- Início de uma linha de dados -->
         <tr>
-            <!-- Cada 'td' representa uma célula da linha com os dados dos pets -->
-            <td>5858</td> <!-- ID do pet -->
-            <td>Thor</td> <!-- Nome do pet -->
-            <td>Labrador</td> <!-- Raça do pet -->
-            <td>M</td> <!-- Sexo do pet (M para macho) -->
-            <td>11/04/2019</td> <!-- Data de nascimento do pet -->
+            <td>5858</td>
+            <td>Thor</td>
+            <td>Labrador</td>
+            <td>M</td>
+            <td>11/04/2019</td>
         </tr>
     </tbody>
 </table>
