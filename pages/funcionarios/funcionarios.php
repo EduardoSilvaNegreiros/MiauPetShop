7347c327a2173491392796b171cd20803bd5cd21
eduardo <edunegreiross@gmail.com>
2024-09-29 21:10:29 -0300

    Atualização funcionarios.php

--- pages/funcionarios/funcionarios.php
+++ pages/funcionarios/funcionarios.php
@@ -1,1 +1,27 @@
 <h3>Funcionários</h3>
+
+<div>
+    <a href="index.php?menuop=funcionarios">Novo Funcionário</a>
+</div>
+
+<!-- Tabela de funcionários -->
+<table>
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Sexo</th>
+            <th>Data de Nasc.</th>
+            <th>Area</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    </tbody>
+</table>
