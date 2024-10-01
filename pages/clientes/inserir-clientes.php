ce9eae863933b84b9c9ace1bdf9ea486efb2362f
eduardo <edunegreiross@gmail.com>
2024-10-01 11:19:26 -0300

    Tentativa de debug

--- pages/clientes/inserir-clientes.php
+++ pages/clientes/inserir-clientes.php
@@ -14,23 +14,24 @@ $telefoneCliente = mysqli_real_escape_string($conexao, $_POST["telefoneCliente"]
 
 // Inserção na tabela tbClientes
 $sql = "INSERT INTO tbclientes (
-    nomeCliente, 
-    emailCliente, 
-    dataNascCliente, 
-    cpfCliente, 
-    sexoCliente, 
+    cpfCliente,
+    dataNascCliente,
+    emailCliente,
+    nomeCliente,
+    sexoCliente,
     telefoneCliente) 
-    VALUES (
-    '{$nomeCliente}',
-    '{$emailCliente}',
-    '{$dataNascCliente}',
+    VALUES(
     '{$cpfCliente}',
+    '{$dataNascCliente}',
+    '{$emailCliente}',
+    '{$nomeCliente}',
     '{$sexoCliente}',
     '{$telefoneCliente}',
-    )";
+    )
+    ";
 
 // Executa a instrução SQL
-mysqli_query($conexao, $sql) or die("Erro ao executar a consulta!" . mysqli_error($conexao));
+mysqli_query($conexao, $sql) or die("Erro ao executar a consulta!. " . mysqli_error($conexao));
 
 echo "O registro foi inserido com sucesso!";
 ?>
