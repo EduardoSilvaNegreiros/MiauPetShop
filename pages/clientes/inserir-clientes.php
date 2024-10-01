4dea8f2a90d6d73cbef517d3542ef9703f50569b
eduardo <edunegreiross@gmail.com>
2024-10-01 11:59:09 -0300

    Inserção de comentários em inserir-clientes.php

--- pages/clientes/inserir-clientes.php
+++ pages/clientes/inserir-clientes.php
@@ -1,36 +1,42 @@
+<!-- /**
+* Inserir Clientes
+*
+* Esta seção do código é responsável por receber os dados do formulário e
+* inserir os mesmos na tabela tbClientes do banco de dados.
+*/ -->
+
 <header>
     <h3>Inserir Clientes</h3>
 </header>
 
 <?php
-// Seleção dos campos do formulário, usando 'mysqli_real_escape_string' para prevenir SQLInjection
-$nomeCliente = mysqli_real_escape_string($conexao, $_POST["nomeCliente"]);
-$emailCliente = mysqli_real_escape_string($conexao, $_POST["emailCliente"]);
-$dataNascCliente = mysqli_real_escape_string($conexao, $_POST["dataNascCliente"]);
-$cpfCliente = mysqli_real_escape_string($conexao, $_POST["cpfCliente"]);
-$sexoCliente = mysqli_real_escape_string($conexao, $_POST["sexoCliente"]);
-$telefoneCliente = mysqli_real_escape_string($conexao, $_POST["telefoneCliente"]);
-$enderecoCliente = mysqli_real_escape_string($conexao, $_POST["endercoCliente"]);
+// Recebe os dados do formulário e protege contra injeção SQL
+$nomeCliente = mysqli_real_escape_string($conexao, $_POST["nomeCliente"]); // Nome do cliente
+$emailCliente = mysqli_real_escape_string($conexao, $_POST["emailCliente"]); // E-mail do cliente
+$dataNascCliente = mysqli_real_escape_string($conexao, $_POST["dataNascCliente"]); // Data de nascimento do cliente
+$cpfCliente = mysqli_real_escape_string($conexao, $_POST["cpfCliente"]); // CPF do cliente
+$sexoCliente = mysqli_real_escape_string($conexao, $_POST["sexoCliente"]); // Sexo do cliente
+$telefoneCliente = mysqli_real_escape_string($conexao, $_POST["telefoneCliente"]); // Telefone do cliente
+$enderecoCliente = mysqli_real_escape_string($conexao, $_POST["endercoCliente"]); // Endereço do cliente
 
-
-// Inserção na tabela tbClientes
+// Constrói a instrução SQL para inserir os dados na tabela tbClientes
 $sql = "INSERT INTO tbclientes (
-    nomeCliente,
-    emailCliente,
-    telefoneCliente,
-    enderecoCliente,
-    sexoCliente,
+    nomeCliente, 
+    emailCliente, 
+    telefoneCliente, 
+    enderecoCliente, 
+    sexoCliente, 
     dataNascCliente,
-    cpfCliente)
-    VALUES(
-    '{$nomeCliente}',
-    '{$emailCliente}',
-    '{$telefoneCliente}',
-    '{$enderecoCliente}',
+    cpfCliente 
+) VALUES (
+    '{$nomeCliente}', 
+    '{$emailCliente}', 
+    '{$telefoneCliente}', 
+    '{$enderecoCliente}', 
     '{$sexoCliente}',
-    '{$dataNascCliente}',
+    '{$dataNascCliente}', 
     '{$cpfCliente}'
-    )";
+)";
 
 // Executa a instrução SQL
 mysqli_query($conexao, $sql) or die("Erro ao executar a consulta!. " . mysqli_error($conexao));
