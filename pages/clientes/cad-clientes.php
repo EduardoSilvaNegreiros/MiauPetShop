482aca475624bb72a3f049a0d20abd0802e44368
eduardo <edunegreiross@gmail.com>
2024-10-01 10:57:30 -0300

    Organização cad-clientes.php

--- pages/clientes/cad-clientes.php
+++ pages/clientes/cad-clientes.php
@@ -7,16 +7,9 @@
     <div class="input-group">
         <!-- Grupo de inputs -->
         <div class="input-box">
-
-
             <div class="input-box">
-                <label for="nomeCliente">Nome</label>
-                <input type="text" name="nomeCliente" id="nomeCliente" required> <!-- Campo para nome -->
-            </div>
-
-            <div class="input-box">
-                <label for="emailCliente">E-mail</label>
-                <input type="email" name="emailCliente" id="emailCliente" required> <!-- Campo para e-mail -->
+                <label for="cpfCliente">CPF</label>
+                <input type="text" name="cpfCliente" id="cpfCliente" required> <!-- Campo para CPF -->
             </div>
 
 
@@ -26,22 +19,28 @@
                 <!-- Campo para data de nascimento -->
             </div>
 
-            <label for="cpfCliente">CPF</label>
-            <input type="text" name="cpfCliente" id="cpfCliente" required> <!-- Campo para CPF -->
-        </div>
+            <div class="input-box">
+                <label for="emailCliente">E-mail</label>
+                <input type="email" name="emailCliente" id="emailCliente" required> <!-- Campo para e-mail -->
+            </div>
 
 
-        <div class="input-box">
-            <label for="sexoCliente">Sexo</label>
-            <input type="text" name="sexoCliente" id="sexoCliente">
-        </div>
+            <div class="input-box">
+                <label for="nomeCliente">Nome</label>
+                <input type="text" name="nomeCliente" id="nomeCliente" required> <!-- Campo para nome -->
+            </div>
 
-        <div class="input-box">
-            <label for="telefoneCliente">Telefone</label>
-            <input type="text" name="telefoneCliente" id="telefoneCliente" required> <!-- Campo para telefone -->
+            <div class="input-box">
+                <label for="sexoCliente">Sexo</label>
+                <input type="text" name="sexoCliente" id="sexoCliente">
+            </div>
+
+            <div class="input-box">
+                <label for="telefoneCliente">Telefone</label>
+                <input type="text" name="telefoneCliente" id="telefoneCliente" required> <!-- Campo para telefone -->
+            </div>
         </div>
-    </div>
-    <div>
-        <input type="submit" value="Adicionar" name="btnAdicionar" class="button-add"> <!-- Botão de envio -->
-    </div>
+        <div>
+            <input type="submit" value="Adicionar" name="btnAdicionar" class="button-add"> <!-- Botão de envio -->
+        </div>
 </form>
--- pages/clientes/inserir-clientes.php
+++ pages/clientes/inserir-clientes.php
@@ -3,8 +3,13 @@
 </header>
 
 <?php 
-    
-    
+    // Seleção dos campos do formulário
     $nomeCliente = mysqli_real_escape_string($conexao, $_POST["nomeCliente"]);
+    $emailCliente = mysqli_real_escape_string($conexao, $_POST["emailCliente"]);
+    $dataNascCliente = mysqli_real_escape_string($conexao, $_POST["dataNascCliente"]);
+    $cpfCliente = mysqli_real_escape_string($conexao, $_POST["cpfCliente"]);
+    $sexoCliente = mysqli_real_escape_string($conexao, $_POST["sexoCliente"]);
+    $telefoneCliente = mysqli_real_escape_string($conexao, $_POST["telefoneCliente"]);
+    
     $sql = 
 ?>
