1f91110b3fec136d1ac38884e278bca2bfb52688
eduardo <edunegreiross@gmail.com>
2024-10-01 11:25:27 -0300

    Organização de ca-clientes.php

--- pages/clientes/cad-clientes.php
+++ pages/clientes/cad-clientes.php
@@ -7,27 +7,21 @@
     <div class="input-group">
         <!-- Grupo de inputs -->
         <div class="input-box">
-            <div class="input-box">
-                <label for="cpfCliente">CPF</label>
-                <input type="text" name="cpfCliente" id="cpfCliente" required> <!-- Campo para CPF -->
-            </div>
-
 
             <div class="input-box">
-                <label for="dataNascCliente">Data de Nascimento</label>
-                <input type="date" name="dataNascCliente" id="dataNascCliente" required>
-                <!-- Campo para data de nascimento -->
+                <label for="nomeCliente">Nome</label>
+                <input type="text" name="nomeCliente" id="nomeCliente" required> <!-- Campo para nome -->
             </div>
+
 
             <div class="input-box">
                 <label for="emailCliente">E-mail</label>
                 <input type="email" name="emailCliente" id="emailCliente" required> <!-- Campo para e-mail -->
             </div>
 
-
             <div class="input-box">
-                <label for="nomeCliente">Nome</label>
-                <input type="text" name="nomeCliente" id="nomeCliente" required> <!-- Campo para nome -->
+                <label for="telefoneCliente">Telefone</label>
+                <input type="text" name="telefoneCliente" id="telefoneCliente" required> <!-- Campo para telefone -->
             </div>
 
             <div class="input-box">
@@ -36,10 +30,17 @@
             </div>
 
             <div class="input-box">
-                <label for="telefoneCliente">Telefone</label>
-                <input type="text" name="telefoneCliente" id="telefoneCliente" required> <!-- Campo para telefone -->
+                <label for="dataNascCliente">Data de Nascimento</label>
+                <input type="date" name="dataNascCliente" id="dataNascCliente" required>
+                <!-- Campo para data de nascimento -->
+            </div>
+
+            <div class="input-box">
+                <label for="cpfCliente">CPF</label>
+                <input type="text" name="cpfCliente" id="cpfCliente" required> <!-- Campo para CPF -->
             </div>
         </div>
+
         <div>
             <input type="submit" value="Adicionar" name="btnAdicionar" class="button-add"> <!-- Botão de envio -->
         </div>
