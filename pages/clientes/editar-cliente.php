1d7e1ba3749d7d903ad3341ad1f984c28552ff3c
eduardo <edunegreiross@gmail.com>
2024-10-09 16:03:46 -0300

    Inserindo a clausula WHERE

--- pages/clientes/editar-cliente.php
+++ pages/clientes/editar-cliente.php
@@ -1,7 +1,8 @@
 <?php 
 $idCliente = $_GET['idCliente'];
-$sql = "SELECT * FROM tbclientes";
-$rs = mysqli_query($conexao, $sql);
+$sql = "SELECT * FROM tbclientes WHERE idCliente= {$idCliente}";
+$rs = mysqli_query($conexao, $sql) or die ('Erro ao recuperar os dados do registro' . mysqli_error($conexao));
+$dados = ;
 ?>
 
 <header>
