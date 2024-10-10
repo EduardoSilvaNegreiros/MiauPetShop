10d61abc0d33f6a4a45d382e2adf44e553b9367f
eduardo <edunegreiross@gmail.com>
2024-10-10 14:31:50 -0300

    Adicionando a clausula WHERE

--- pages/clientes/atualizar-cliente.php
+++ pages/clientes/atualizar-cliente.php
@@ -20,11 +20,11 @@ $sql = "UPDATE tbclientes SET
   sexoCliente = {$sexoCliente},
   telefoneCliente = {$telefoneCliente},
   enderecoCliente = {$enderecoCliente},
+  WHERE idCliente = {$idCliente},
 ";
 
-// Executa a instrução SQL
 mysqli_query($conexao, $sql) or die("Erro ao executar a consulta!. " . mysqli_error($conexao));
 
-// Caso ocorra tudo corretamente, aparecera está msg de sucesso
+
 echo "O registro foi inserido com sucesso!";
 ?>
