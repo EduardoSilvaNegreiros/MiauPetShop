7ae209a8736aa8fadf2b5cf0926c4d1ed9363dc5
eduardo <edunegreiross@gmail.com>
2024-10-10 14:27:32 -0300

    Inserção de código no atualizar-cliente.php

---
+++ pages/clientes/atualizar-cliente.php
@@ -0,0 +1,39 @@
+<header>
+    <h3>Atualizar Cliente</h3>
+</header>
+
+<?php
+$idCliente = mysqli_real_escape_string($conexao, $_POST["idCliente"]);
+$nomeCliente = mysqli_real_escape_string($conexao, $_POST["nomeCliente"]); // Nome do cliente
+$emailCliente = mysqli_real_escape_string($conexao, $_POST["emailCliente"]); // E-mail do cliente
+$dataNascCliente = mysqli_real_escape_string($conexao, $_POST["dataNascCliente"]); // Data de nascimento do cliente
+$cpfCliente = mysqli_real_escape_string($conexao, $_POST["cpfCliente"]); // CPF do cliente
+$sexoCliente = mysqli_real_escape_string($conexao, $_POST["sexoCliente"]); // Sexo do cliente
+$telefoneCliente = mysqli_real_escape_string($conexao, $_POST["telefoneCliente"]); // Telefone do cliente
+$enderecoCliente = mysqli_real_escape_string($conexao, $_POST["enderecoCliente"]); // Endereço do cliente
+
+// Constrói a instrução SQL para inserir os dados na tabela tbClientes
+$sql = "INSERT INTO tbclientes (
+    nomeCliente, 
+    emailCliente, 
+    telefoneCliente, 
+    enderecoCliente, 
+    sexoCliente, 
+    dataNascCliente,
+    cpfCliente 
+) VALUES (
+    '{$nomeCliente}', 
+    '{$emailCliente}', 
+    '{$telefoneCliente}', 
+    '{$enderecoCliente}', 
+    '{$sexoCliente}',
+    '{$dataNascCliente}', 
+    '{$cpfCliente}'
+)";
+
+// Executa a instrução SQL
+mysqli_query($conexao, $sql) or die("Erro ao executar a consulta!. " . mysqli_error($conexao));
+
+// Caso ocorra tudo corretamente, aparecera está msg de sucesso
+echo "O registro foi inserido com sucesso!";
+?>
