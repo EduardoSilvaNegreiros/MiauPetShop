37c84794269306f19afc5190311a6381e782b15e
eduardo <edunegreiross@gmail.com>
2024-10-14 10:11:41 -0300

    Organização de inserir-funcionario.php

--- pages/funcionarios/inserir-funcionarios.php
+++ pages/funcionarios/inserir-funcionarios.php
@@ -1,10 +1,3 @@
-<!-- /**
-* Inserir Funcionários
-*
-* Esta seção do código é responsável por receber os dados do formulário e
-* inserir os mesmos na tabela Funcionarios do banco de dados.
-*/ -->
-
 <header>
     <h3>Inserir Funcionários</h3>
 </header>
@@ -12,16 +5,17 @@
 <?php
 // Recebe os dados do formulário e protege contra injeção SQL
 $nomeFuncionario = mysqli_real_escape_string($conexao, $_POST["nomeFuncionario"]); // Nome do funcionário
+$cpfFuncionario = mysqli_real_escape_string($conexao, $_POST["cpfFuncionario"]); // Status do funcionário
 $emailFuncionario = mysqli_real_escape_string($conexao, $_POST["emailFuncionario"]); // E-mail do funcionário
 $telefoneFuncionario = mysqli_real_escape_string($conexao, $_POST["telefoneFuncionario"]); // Telefone do funcionário
 $cargoFuncionario = mysqli_real_escape_string($conexao, $_POST["cargoFuncionario"]); // Cargo do funcionário
 $dataContratacao = mysqli_real_escape_string($conexao, $_POST["dataContratacao"]); // Data de contratação
 $salarioFuncionario = mysqli_real_escape_string($conexao, $_POST["salarioFuncionario"]); // Salário do funcionário
 $statusFuncionario = mysqli_real_escape_string($conexao, $_POST["statusFuncionario"]); // Status do funcionário
-
 // Constrói a instrução SQL para inserir os dados na tabela Funcionarios
-$sql = "INSERT INTO Funcionarios (
+$sql = "INSERT INTO tbfuncionarios (
     nomeFuncionario, 
+    cpfFuncionario,
     emailFuncionario, 
     telefoneFuncionario, 
     cargoFuncionario, 
@@ -30,6 +24,7 @@ $sql = "INSERT INTO Funcionarios (
     statusFuncionario
 ) VALUES (
     '{$nomeFuncionario}', 
+    '{$cpfFuncionario}', 
     '{$emailFuncionario}', 
     '{$telefoneFuncionario}', 
     '{$cargoFuncionario}', 
