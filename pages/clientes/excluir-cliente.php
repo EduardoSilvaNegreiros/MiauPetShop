c5a7b8ea20025804ea787806002bb32f43ef9fec
eduardo <edunegreiross@gmail.com>
2024-10-14 09:34:02 -0300

    Adicionando código para a exclusão de clientes em excluir-cliente.php

--- pages/clientes/excluir-cliente.php
+++ pages/clientes/excluir-cliente.php
@@ -2,5 +2,9 @@
   <h3>Excluir Cliente</h3>
 </header>
 <?php 
-  
+  $idCliente = mysqli_real_escape_string($conexao, $_GET['idCliente']);
+  $sql = "DELETE FROM tbclientes WHERE idCliente = '{idCliente}'";
+
+  mysqli_query($conexao, $sql) or die("Erro ao excluir o registro." . mysqli_error($conexao));
+  echo "Registro excluido com sucesso";
 ?>
