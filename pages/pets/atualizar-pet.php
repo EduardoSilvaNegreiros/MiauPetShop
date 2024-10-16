9e94e9ff7348b6bc9206ff1b8593fb6e442e62cf
eduardo <edunegreiross@gmail.com>
2024-10-16 09:24:20 -0300

    Atualização atualizar-pet.php

--- pages/pets/atualizar-pet.php
+++ pages/pets/atualizar-pet.php
@@ -3,22 +3,22 @@
 </header>
 
 <?php
-$idPet = mysqli_real_escape_string($conexao, $_POST["idPet"]); // Id do Pet
-$nomePet = mysqli_real_escape_string($conexao, $_POST["nomePet"]); // Nome do Pet
-$tipoPet = mysqli_real_escape_string($conexao, $_POST["tipoPet"]); // Tipo do Pet (Cachorro, Gato, etc.)
-$racaPet = mysqli_real_escape_string($conexao, $_POST["racaPet"]); // Raça do Pet
-$idadePet = mysqli_real_escape_string($conexao, $_POST["idadePet"]); // Idade do Pet
-$sexoPet = mysqli_real_escape_string($conexao, $_POST["sexoPet"]); // Sexo do Pet
-$pesoPet = mysqli_real_escape_string($conexao, $_POST["pesoPet"]); // Peso do Pet
+$idPet = mysqli_real_escape_string($conexao, $_POST["idPet"]);
+$nomePet = mysqli_real_escape_string($conexao, $_POST["nomePet"]);
+$tipoPet = mysqli_real_escape_string($conexao, $_POST["tipoPet"]);
+$racaPet = mysqli_real_escape_string($conexao, $_POST["racaPet"]);
+$idadePet = mysqli_real_escape_string($conexao, $_POST["idadePet"]);
+$sexoPet = mysqli_real_escape_string($conexao, $_POST["sexoPet"]);
+$pesoPet = mysqli_real_escape_string($conexao, $_POST["pesoPet"]);
 
 $sql = "UPDATE tbPets SET
-  nomePet = '{$nomePet}',
-  tipoPet = '{$tipoPet}',
-  racaPet = '{$racaPet}',
-  idadePet = '{$idadePet}',
-  sexoPet = '{$sexoPet}',
-  pesoPet = '{$pesoPet}'
-  WHERE idPet = '{$idPet}'
+    nomePet = '{$nomePet}',
+    tipoPet = '{$tipoPet}',
+    racaPet = '{$racaPet}',
+    idadePet = '{$idadePet}',
+    sexoPet = '{$sexoPet}',
+    pesoPet = '{$pesoPet}'
+    WHERE idPet = '{$idPet}'
 ";
 
 mysqli_query($conexao, $sql) or die("Erro ao executar a consulta! " . mysqli_error($conexao));
