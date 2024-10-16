d6134a04da36f64f78306271b8f83bf8c09f8d51
eduardo <edunegreiross@gmail.com>
2024-10-16 09:31:06 -0300

    Atualização de excluir-pet.php

---
+++ pages/pets/excluir-pet.php
@@ -0,0 +1,11 @@
+<header>
+    <h3>Excluir Pet</h3>
+</header>
+
+<?php 
+$idPet = mysqli_real_escape_string($conexao, $_GET["idPet"]);
+$sql = "DELETE FROM tbPets WHERE idPet = '$idPet'";
+
+mysqli_query($conexao, $sql) or die("Erro ao excluir o registro: " . mysqli_error($conexao));
+echo "Registro excluído com sucesso!";
+?>
