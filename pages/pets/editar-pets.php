196b0036fa50c5fe37f7e841665271030a7de995
eduardo <edunegreiross@gmail.com>
2024-10-15 23:26:37 -0300

    Atualização de editar-pets.php

--- pages/pets/editar-pets.php
+++ pages/pets/editar-pets.php
@@ -1,20 +1,61 @@
 <?php 
 $idPet = $_GET['idPet'];
-$sql = "SELECT * FROM tbpets WHERE idPet= {$idPet}";
-$rs = mysqli_query($conexao, $sql) or die ('Erro ao recuperar os dados do registro' . mysqli_error($conexao));
+$sql = "SELECT * FROM tbPets WHERE idPet= {$idPet}";
+$rs = mysqli_query($conexao, $sql) or die ('Erro ao recuperar os dados do registro: ' . mysqli_error($conexao));
 $dados = mysqli_fetch_assoc($rs);
 ?>
 
 <header>
-  <h3>Editar Pets</h3>
+  <h3>Editar Pet</h3>
 </header>
 
 <form action="index.php?menuop=atualizar-pet" method="POST" class="form">
 
     <div class="input-group">
 
-        <div>
-            <input type="submit" value="Atualizar" name="btnAtualizar" class="button-add"> 
+        <div class="input-box">
+            <label for="idPet">ID</label>
+            <input type="text" name="idPet" id="idPet" value="<?=$dados['idPet']?>" readonly> <!-- Campo para id (readonly) -->
         </div>
 
+        <div class="input-box">
+            <label for="nomePet">Nome</label>
+            <input type="text" name="nomePet" id="nomePet" value="<?=$dados['nomePet']?>" required> <!-- Campo para nome -->
+        </div>
+
+        <div class="input-box">
+            <label for="tipoPet">Tipo</label>
+            <input type="text" name="tipoPet" id="tipoPet" value="<?=$dados['tipoPet']?>" required> <!-- Campo para tipo -->
+        </div>
+
+        <div class="input-box">
+            <label for="racaPet">Raça</label>
+            <input type="text" name="racaPet" id="racaPet" value="<?=$dados['racaPet']?>" required> <!-- Campo para raça -->
+        </div>
+
+        <div class="input-box">
+            <label for="sexoPet">Sexo</label>
+            <select name="sexoPet" id="sexoPet" required>
+                <option value="" disabled>Selecione</option>
+                <option value="masculino" <?= ($dados['sexoPet'] == 'masculino') ? 'selected' : '' ?>>Masculino</option>
+                <option value="feminino" <?= ($dados['sexoPet'] == 'feminino') ? 'selected' : '' ?>>Feminino</option>
+            </select>
+        </div>
+
+        <div class="input-box">
+            <label for="dataNascPet">Data de Nascimento</label>
+            <input type="date" name="dataNascPet" id="dataNascPet" value="<?=$dados['dataNascPet']?>" required>
+            <!-- Campo para data de nascimento -->
+        </div>
+
+        <div class="input-box">
+            <label for="cpfDono">CPF do Dono</label>
+            <input type="text" name="cpfDono" id="cpfDono" value="<?=$dados['cpfDono']?>" required> <!-- Campo para CPF do dono -->
+        </div>
+
+    </div>
+
+    <div>
+        <input type="submit" value="Atualizar" name="btnAtualizar" class="button-add"> 
+    </div>
 </form>
