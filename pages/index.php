914f16a9f4990e8bbcee733cbbb5357015487cef
eduardo <edunegreiross@gmail.com>
2024-10-17 10:09:50 -0300

    Atualização de servicos.php

--- pages/index.php
+++ pages/index.php
@@ -81,7 +81,7 @@ $menuop = isset($_GET["menuop"]) ? $_GET["menuop"] : 'funcionarios'; // Valor pa
                         include("servicos/servicos.php");
                         break;
 
-                    case 'cad-servico':
+                    case 'cad-servicos':
                         include("servicos/cad-servicos.php");
                         break;
 
