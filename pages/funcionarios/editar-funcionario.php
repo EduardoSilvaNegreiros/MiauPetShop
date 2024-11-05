04608493faccfab8bbe0a841a5975149809f0c49
EduardoSilvaNegreiros <edunegreiross@gmail.com>
2024-11-05 12:00:00 -0300

    Atualização

--- pages/funcionarios/editar-funcionario.php
+++ pages/funcionarios/editar-funcionario.php
@@ -1,7 +1,7 @@
 <?php
 $idFuncionario = $_GET['idFuncionario'];
 $sql = "SELECT * FROM tbfuncionarios WHERE idFuncionario= {$idFuncionario}";
-$rs = mysqli_query($conexao, $sql) or die('Erro ao recuperar os dados do registro' . mysqli_error($conexao));
+$rs = mysqli_query($conexao, $sql) or die('Erro ao recuperar os dados do registro: ' . mysqli_error($conexao));
 $dados = mysqli_fetch_assoc($rs);
 ?>
 
@@ -14,66 +14,67 @@ $dados = mysqli_fetch_assoc($rs);
     <div class="input-group">
 
         <div class="input-box">
-
-            <div class="input-box">
-                <label for="idFuncionario">ID</label>
-                <input type="text" name="idFuncionario" id="idFuncionario" value="<?= $dados['idFuncionario'] ?>" readonly>
-                <!-- Campo para id -->
-            </div>
-
-            <div class="input-box">
-                <label for="nomeFuncionario">Nome</label>
-                <input type="text" name="nomeFuncionario" id="nomeFuncionario" value="<?= $dados['nomeFuncionario'] ?>">
-                <!-- Campo para nome -->
-            </div>
-
-            <div class="input-box">
-                <label for="cpfFuncionario">CPF</label>
-                <input type="text" name="cpfFuncionario" id="cpfFuncionario" value="<?= $dados['cpfFuncionario'] ?>">
-                <!-- Campo para CPF -->
-            </div>
-
-            <div class="input-box">
-                <label for="emailFuncionario">E-mail</label>
-                <input type="email" name="emailFuncionario" id="emailFuncionario" value="<?= $dados['emailFuncionario'] ?>">
-                <!-- Campo para e-mail -->
-            </div>
-
-            <div class="input-box">
-                <label for="telefoneFuncionario">Telefone</label>
-                <input type="text" name="telefoneFuncionario" id="telefoneFuncionario"
-                    value="<?= $dados['telefoneFuncionario'] ?>"> <!-- Campo para telefone -->
-            </div>
-
-            <div class="input-box">
-                <label for="cargoFuncionario">Cargo</label>
-                <input type="text" name="cargoFuncionario" id="cargoFuncionario" value="<?= $dados['cargoFuncionario'] ?>">
-                <!-- Campo para telefone -->
-            </div>
-
-            <div class="input-box">
-                <label for="dataContratacao">Data de Contratação</label>
-                <input type="text" name="dataContratacao" id="dataContratacao" value="<?= $dados['dataContratacao'] ?>">
-            </div>
-
-            <div class="input-box">
-                <label for="salarioFuncionario">Salário</label>
-                <input type="text" name="salarioFuncionario" id="salarioFuncionario"
-                    value="<?= $dados['salarioFuncionario'] ?>">
-            </div>
-
-            <div class="input-box">
-                <label for="statusFuncionario">Status Funcionário</label>
-                <select name="statusFuncionario" id="statusFuncionario" required>
-                    <option value="">Selecione o status</option>
-                    <option value="Ativo" <?= ($dados['statusFuncionario'] == 'Ativo') ? 'selected' : '' ?>>Ativo</option>
-                    <option value="Inativo" <?= ($dados['statusFuncionario'] == 'Inativo') ? 'selected' : '' ?>>Inativo</option>
-                </select>
-            </div>
-
+            <label for="idFuncionario">ID</label>
+            <input type="text" name="idFuncionario" id="idFuncionario" value="<?= $dados['idFuncionario'] ?>" readonly>
+            <!-- Campo de ID (somente leitura) -->
         </div>
 
-        <div>
-            <input type="submit" value="Atualizar" name="btnAtualizar" class="button-add">
+        <div class="input-box">
+            <label for="nomeFuncionario">Nome</label>
+            <input type="text" name="nomeFuncionario" id="nomeFuncionario" value="<?= $dados['nomeFuncionario'] ?>">
+            <!-- Campo de Nome -->
         </div>
+
+        <div class="input-box">
+            <label for="cpfFuncionario">CPF</label>
+            <input type="text" name="cpfFuncionario" id="cpfFuncionario" value="<?= $dados['cpfFuncionario'] ?>">
+            <!-- Campo de CPF -->
+        </div>
+
+        <div class="input-box">
+            <label for="emailFuncionario">E-mail</label>
+            <input type="email" name="emailFuncionario" id="emailFuncionario" value="<?= $dados['emailFuncionario'] ?>">
+            <!-- Campo de E-mail -->
+        </div>
+
+        <div class="input-box">
+            <label for="telefoneFuncionario">Telefone</label>
+            <input type="text" name="telefoneFuncionario" id="telefoneFuncionario" value="<?= $dados['telefoneFuncionario'] ?>">
+            <!-- Campo de Telefone -->
+        </div>
+
+        <div class="input-box">
+            <label for="cargoFuncionario">Cargo</label>
+            <input type="text" name="cargoFuncionario" id="cargoFuncionario" value="<?= $dados['cargoFuncionario'] ?>">
+            <!-- Campo de Cargo -->
+        </div>
+
+        <div class="input-box">
+            <label for="dataContratacao">Data de Contratação</label>
+            <input type="text" name="dataContratacao" id="dataContratacao" value="<?= $dados['dataContratacao'] ?>">
+            <!-- Campo de Data de Contratação -->
+        </div>
+
+        <div class="input-box">
+            <label for="salarioFuncionario">Salário</label>
+            <input type="text" name="salarioFuncionario" id="salarioFuncionario" value="<?= $dados['salarioFuncionario'] ?>">
+            <!-- Campo de Salário -->
+        </div>
+
+        <div class="input-box">
+            <label for="statusFuncionario">Status Funcionário</label>
+            <select name="statusFuncionario" id="statusFuncionario" required>
+                <option value="">Selecione o status</option>
+                <option value="Ativo" <?= ($dados['statusFuncionario'] == 'Ativo') ? 'selected' : '' ?>>Ativo</option>
+                <option value="Inativo" <?= ($dados['statusFuncionario'] == 'Inativo') ? 'selected' : '' ?>>Inativo</option>
+            </select>
+            <!-- Campo de Status -->
+        </div>
+
+    </div>
+
+    <div>
+        <input type="submit" value="Atualizar" name="btnAtualizar" class="button-add">
+        <!-- Botão de atualização -->
+    </div>
 </form>
